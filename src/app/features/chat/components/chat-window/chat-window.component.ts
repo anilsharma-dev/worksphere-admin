76d17760f6da0eb9deb3554c8b21e4efcae8d935
Anil Sharma <anil4827@gmail.com>
2026-06-10 23:28:13 +0530

    feat: add chat window skeleton loading states

--- src/app/features/chat/components/chat-window/chat-window.component.ts
+++ src/app/features/chat/components/chat-window/chat-window.component.ts
@@ -1,29 +1,56 @@
-import { Component } from '@angular/core';
-
+import {
+  Component,
+  OnInit,
+  signal
+}
+from '@angular/core';
 
 import {
   ChatService,
   Message
-} from '../../../../core/services/chat.service';
+}
+from '../../../../core/services/chat.service';
 
 @Component({
-    selector: 'app-chat-window',
-    imports: [],
-    templateUrl: './chat-window.component.html',
-    styleUrl: './chat-window.component.scss'
+  selector: 'app-chat-window',
+
+  standalone: true,
+
+  imports: [],
+
+  templateUrl:
+    './chat-window.component.html',
+
+  styleUrl:
+    './chat-window.component.scss'
 })
-export class ChatWindowComponent {
+
+export class ChatWindowComponent
+implements OnInit {
 
   messages: Message[] = [];
 
+  loading =
+    signal(true);
+
   constructor(
     private chatService: ChatService
-  ) {
+  ) {}
 
-    this.chatService.messages$
-      .subscribe(messages => {
-        this.messages = messages;
-      });
+  ngOnInit(): void {
+
+    setTimeout(() => {
+
+      this.chatService.messages$
+        .subscribe(messages => {
+
+          this.messages = messages;
+
+          this.loading.set(false);
+
+        });
+
+    }, 1200);
 
   }
 
